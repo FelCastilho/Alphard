d3c77494123b967518532d27ed99caa1a058a942
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2022-08-29 18:07:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Our main file.
 
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    audio = r.listen(source)
+    
+
+    print(r.recognize_google(audio, language='pt'))
